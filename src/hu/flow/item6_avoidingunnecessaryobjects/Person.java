c07e1dd09241b3bde6ce73393e1254215d0cf3de
Tamas Vajgely <vajgi90@gmail.com>
2020-03-29 10:11:44 +0200

    Item6: Avoid the creating of unneccessary objects.

---
+++ src/hu/flow/item6_avoidingunnecessaryobjects/Person.java
@@ -0,0 +1,28 @@
+package hu.flow.item6_avoidingunnecessaryobjects;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class Person {
+  private final Date birthDate;
+  private static final Date BOOM_START;
+  private static final Date BOOM_END;
+
+  static {
+    Calendar gmtCal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
+    gmtCal.set(1946, Calendar.JANUARY, 1, 0, 0, 0);
+    BOOM_START = gmtCal.getTime();
+    gmtCal.set(1965, Calendar.JANUARY, 1, 0, 0, 0);
+    BOOM_END = gmtCal.getTime();
+  }
+
+  public Person(Date birthDate) {
+    this.birthDate = birthDate;
+  }
+
+  public boolean isBabyBoomer() {
+    return birthDate.compareTo(BOOM_START) >= 0 && birthDate.compareTo(BOOM_END) < 0;
+  }
+
+}
