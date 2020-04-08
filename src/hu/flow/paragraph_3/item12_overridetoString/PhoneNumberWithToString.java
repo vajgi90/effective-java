20e06ed0b68bafc9ba687fb0a9bd2560872abe0f
Tamas Vajgely <vajgi90@gmail.com>
2020-04-08 10:01:56 +0200

    String.format() style toString() method

--- src/hu/flow/paragraph_3/item12_overridetoString/PhoneNumberWithToString.java
+++ src/hu/flow/paragraph_3/item12_overridetoString/PhoneNumberWithToString.java
@@ -1,6 +1,7 @@
 package hu.flow.paragraph_3.item12_overridetoString;
 
 public class PhoneNumberWithToString {
+
   private int areaCode, prefix, lineNum;
   private int hashCode;
 
@@ -10,7 +11,7 @@ public class PhoneNumberWithToString {
     this.lineNum = lineNum;
   }
 
-  @Override
+/*  @Override
   public String toString() {
     return "PhoneNumberWithToString{" +
         "areaCode=" + areaCode +
@@ -18,5 +19,20 @@ public class PhoneNumberWithToString {
         ", lineNum=" + lineNum +
         ", hashCode=" + hashCode +
         '}';
+  }*/
+
+  /**
+   * Returns the string representation of this phone number. The string consists of twelve
+   * characters whose format is "XXX-YYY-ZZZZ", where XXX is the area code, YYY is the prefix, and
+   * ZZZZ is the line number. Each of the capital letters represents a single decimal digit.
+   * <p>
+   * If any of the three parts of this phone number is too small to fill up its field, the field is
+   * padded with leading zeros. For example, if the value of the line number is 123, the last four
+   * characters of the string representation will be "0123".
+   */
+  @Override
+  public String toString() {
+    return String.format("%03d-%03d-%04d",
+        areaCode, prefix, lineNum);
   }
 }
